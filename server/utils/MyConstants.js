2b7d2575a58d2abeb90d704dce785c5f191ff88d
lenhan2k3 <nhan.2174802010838@vanlanguni.vn>
2023-11-03 23:10:45 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster.ubemscf.mongodb.net',
+    DB_USER: 'tringuyen8826',
+    DB_PASS: '04122003',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'caigicungduoc',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'tringuyen8826@gmail.com', // gmail service
+    EMAIL_PASS: 'ajityuiqrkarbzly',
+  };
+  module.exports = MyConstants;
